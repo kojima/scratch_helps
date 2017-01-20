a17fc99f752d32d9fa3453db77390569a9cb8a40
Hidenori Kojima <hide.k0712@gmail.com>
2017-01-20 14:51:24 +0900

    Added the Scratch x Kinect programming contents.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 $(function() {
 
+  setup();
+
   $('.item').each(function () {
+    var count = 0;
     $('.sub-item', $(this)).each(function() {
-      if ($(this).index() % 2 == 0) {
+      if ($(this).css('display') == 'none') return;
+
+      if (count % 2 == 0) {
         $(this).addClass('even')
       } else {
         $(this).addClass('odd');
       }
+      count++;
     });
   });
 
@@ -56,5 +62,39 @@ function handleResize() {
       var $ul = $('ul', $item);
       $ul.css('margin-top', -1 * $ul.height());
     }
+  }
+}
+
+function getQueryString() {
+  // This function is anonymous, is executed immediately and
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]], pair[1] ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  }
+    return query_string;
+}
+
+function setup() {
+  var queryString = getQueryString();
+  if (queryString['kinect'] === 'true') {
+    $('.scratch-only').hide();
+    $('.scratch-x-kinect').show();
+  } else {
+    $('.scratch-only').show();
+    $('.scratch-x-kinect').hide();
   }
 }
